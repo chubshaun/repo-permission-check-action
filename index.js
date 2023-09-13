95e6ad72a5b3caf89b5180a2bc0fd3726f0ab0b9
chubshaun <105888480+chubshaun@users.noreply.github.com>
2023-09-13 15:49:23 -0500

    Added fake permissions for testing

--- index.js
+++ index.js
@@ -22,11 +22,17 @@ const username = github.context.actor;
   core.debug(`[Action] User Permission: ${permission}`);
   core.debug(`[Action] Minimum Action Permission: ${argPerm}`);
 
+  core.info(`Actor: ${username}`);
+  core.info(`User's Current Permission Level: ${permission}`);
+  core.info(`Required Permission Level for Action: ${argPerm}`);
+
   // If the index of your permission is at least or greater than the required,
   // exit successfully. Otherwise fail.
   if (yourPermIdx >= requiredPermIdx) {
+    core.info(`[Action] User has sufficient permissions.`);
     process.exit(0);
   } else {
+    core.error(`[Action] User does not have sufficient permissions.`);
     process.exit(1);
   }
 })();
